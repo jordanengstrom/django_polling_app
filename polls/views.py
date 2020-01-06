e9b153a1103b0df34beb9f9680ce436aa9260635
Jordan Engstrom <jordan.engstrom@gmail.com>
2020-01-05 17:50:50 -0700

    part 6: added static files for css

--- polls/views.py
+++ polls/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.template import loader
 from django.views import generic
+from django.utils import timezone
 
 from .models import Choice, Question
 
@@ -14,17 +15,24 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
 
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
 
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
+
 
 # Function views (writing views the hard way):
 # def index(request):
